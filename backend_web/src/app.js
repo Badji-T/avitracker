1f648f9fc42aa64e53be91f0fefeb4baad79f4f8
= <=>
2026-04-28 16:05:30 +0000

    environnement connecte a la Bdd mysql hebergee sur railway

--- backend_web/src/app.js
+++ backend_web/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const db = require('./db/database-con');
 
 const app = express();
 
@@ -10,6 +11,13 @@ app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.send('API Avitracker OK');
+});
+
+app.get("/", (req, res) => {
+  db.query("SELECT 1", (err, result) => {
+    if (err) return res.send(err);
+    res.send("API + DB OK ");
+  });
 });
 
 module.exports = app;
---
+++ backend_web/src/db/database-con.js
@@ -0,0 +1,23 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log("Erreur connexion DB:", err);
+  } else {
+    console.log("Connecté à Railway MySQL DB");
+  }
+});
+
+module.exports = db;
